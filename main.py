b6eb1d650ebf8d43d5450814191470df8ad9cb4f
ellen <chenellen50@gmail.com>
2024-02-24 20:38:56 +0100

    Error handling

--- main.py
+++ main.py
@@ -9,26 +9,29 @@ days = st.slider("Forecast Days:", min_value=1, max_value=5,
                  help="Select the number of forecasted days")
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 
-# Get temperatures and sky data
-if place:
-    st.subheader(f"{option} for the next {days} day(s) in {place.title()}")
-    dates, filtered_data = get_data(place, days)
+try:
+    # Get temperatures and sky data
+    if place:
+        dates, filtered_data = get_data(place, days)
+        st.subheader(f"{option} for the next {days} day(s) in {place.title()}")
 
-    # Display temperatures
-    if option == "Temperature":
-        temperatures = [dict["main"]["temp"] for dict in filtered_data]
-        figure = px.line(x=dates, y=temperatures,
-                         labels={"x": "Date", "y": "Temperature (C)"})
-        st.plotly_chart(figure)
-    else:
-        # Get the images
-        images = {"Clouds": "images/cloud.png",
-                  "Rain": "images/rain.png",
-                  "Snow": "images/snow.png",
-                  "Clear": "images/clear.png"}
+        # Display temperatures
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures,
+                             labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
+        else:
+            # Get the images
+            images = {"Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png",
+                      "Snow": "images/snow.png",
+                      "Clear": "images/clear.png"}
 
-        # Display sky
-        sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
-        image_paths = [images[condition] for condition in sky_conditions]
+            # Display sky
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions]
 
-        st.image(image_paths, caption=dates, width=200)
+            st.image(image_paths, caption=dates, width=200)
+except KeyError:
+    st.error("The city name is incorrect. Please try again.")
